934d2e8803b6b539ae9f16d1c562b31333b05002
George Tsiokos <gtsiokos@gmail.com>
2024-05-13 18:28:00 +0300

    initial

--- wps-product-search-field-exclude-attributes.php
+++ wps-product-search-field-exclude-attributes.php
@@ -16,17 +16,11 @@ if ( !defined( 'ABSPATH' ) ) {
 class WPS_Product_Search_Field_Exclude_Attributes {
 
 	public static function init() {
-		//add_action( 'wp_ajax_product_search', array( __CLASS__, 'wp_ajax_product_search' ), 9 );
-		//add_action( 'wp_ajax_nopriv_product_search', array( __CLASS__, 'wp_ajax_product_search' ), 9 );
 		add_action('woocommerce_product_search_service_post_ids_for_request', array( __CLASS__, 'woocommerce_product_search_service_post_ids_for_request' ), 10, 2 );
 	}
 
-	public static function wp_ajax_product_search() {
-		//add_action('woocommerce_product_search_service_post_ids_for_request', array( __CLASS__, 'woocommerce_product_search_service_post_ids_for_request' ), 10, 2 );
-	}
-
 	public static function woocommerce_product_search_service_post_ids_for_request( &$products, $context ) {
-		$excluded_attributes = apply_filters( 'wps-product-search-field-exclude-attributes', array( 'pa_brand' ) );
+		$excluded_attributes = array( 'pa_brand' );
 		foreach ( $products as $key => $product_id ) {
 			$product = wc_get_product( $product_id );
 			if ( $product ) {
